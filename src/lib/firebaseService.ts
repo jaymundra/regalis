1bb2e72588b56b6d0569ec1d8bba3729ee4b4969
Jay Mundra <jay@Jays-MacBook-Air.local>
2025-10-19 19:36:02 +0530

    commit

--- src/lib/firebaseService.ts
+++ src/lib/firebaseService.ts
@@ -6,6 +6,9 @@ export interface WaitlistData {
   email: string;
   selectedProducts: Array<{
     name: string;
+    images: string[];
+    price: string;
+    discounted: string;
   }>;
   submittedAt: Timestamp;
 }
@@ -13,8 +16,11 @@ export interface WaitlistData {
 export const addToWaitlist = async (data: Omit<WaitlistData, 'submittedAt'>) => {
   try {
     // console.error('Adding data to firebase', data);
+    const selectedProductNames = data.selectedProducts.map(product => product.name);
+
     const docRef = await addDoc(collection(db, 'waitlist'), {
       ...data,
+      selectedProducts: selectedProductNames,  // store only names
       submittedAt: Timestamp.now(),
     });
     // console.error('Added data to firebase', data);
